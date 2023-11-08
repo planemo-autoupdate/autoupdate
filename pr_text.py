e9b5db8644abc4f557c62dd8dc4513a495b24084
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2023-11-08 12:11:32 +0000

    Format with black

--- pr_text.py
+++ pr_text.py
@@ -8,10 +8,10 @@ import yaml
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--repo', help='Tool repo')
-parser.add_argument('--log', help='Autoupdate log')
-parser.add_argument('--shed', help='Location of .shed.yml file input.')
-parser.add_argument('--out', help='Output file.')
+parser.add_argument("--repo", help="Tool repo")
+parser.add_argument("--log", help="Autoupdate log")
+parser.add_argument("--shed", help="Location of .shed.yml file input.")
+parser.add_argument("--out", help="Output file.")
 args = parser.parse_args()
 
 with open(args.log) as f:
@@ -31,26 +31,32 @@ with open(args.log) as f:
 
 text = []
 
-text.append(f"Hello! This is an automated update of the following tool: **{args.repo}**. I created this PR because I think the tool's main dependency is out of date, i.e. there is a newer version available through conda.")
+text.append(
+    f"Hello! This is an automated update of the following tool: **{args.repo}**. I created this PR because I think the tool's main dependency is out of date, i.e. there is a newer version available through conda."
+)
 
 text.append(f"I have updated {args.repo} {update}.")
 
 with open(args.shed) as f:
     y = yaml.load(f, Loader=yaml.SafeLoader)
 
-if y.get('homepage_url'):
-    url = y.get('homepage_url').strip('/')
-    if 'github.com' in url:
-        if len(url.split('github.com')[1].split('/')) > 1:
-            url += '/releases'
-    text.append(f'**Project home page:** {url}')
+if y.get("homepage_url"):
+    url = y.get("homepage_url").strip("/")
+    if "github.com" in url:
+        if len(url.split("github.com")[1].split("/")) > 1:
+            url += "/releases"
+    text.append(f"**Project home page:** {url}")
 
-if y.get('maintainers'):
-    text.append('**Maintainers:** ' + ', '.join([f'@{m}' for m in y.get('maintainers')]))
+if y.get("maintainers"):
+    text.append(
+        "**Maintainers:** " + ", ".join([f"@{m}" for m in y.get("maintainers")])
+    )
 
-text.append("For any comments, queries or criticism about the bot, not related to the tool being updated in this PR, please create an issue [here](https://github.com/planemo-autoupdate/autoupdate/issues/new).")
+text.append(
+    "For any comments, queries or criticism about the bot, not related to the tool being updated in this PR, please create an issue [here](https://github.com/planemo-autoupdate/autoupdate/issues/new)."
+)
 
-with open(args.out, 'w') as f:
-    f.write('\n\n'.join(text))
+with open(args.out, "w") as f:
+    f.write("\n\n".join(text))
 
-print(f'Updating {args.repo} {update}')
+print(f"Updating {args.repo} {update}")
--- pr_text_iwc.py
+++ pr_text_iwc.py
@@ -9,41 +9,56 @@ from datetime import date
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--repo', help='Tool repo')
-parser.add_argument('--log', help='Autoupdate log')
-# parser.add_argument('--shed', help='Location of .shed.yml file input.')
-parser.add_argument('--out', help='Output file.')
-parser.add_argument('--changelog', help='Changelog location')
-parser.add_argument('--pr-exists', type=int, help='Whether a PR already exists')  # then we don't need to update the release notes
+parser.add_argument("--repo", help="Tool repo")
+parser.add_argument("--log", help="Autoupdate log")
+# parser.add_argument("--shed", help="Location of .shed.yml file input.")
+parser.add_argument("--out", help="Output file.")
+parser.add_argument("--changelog", help="Changelog location")
+parser.add_argument(
+    "--pr-exists", type=int, help="Whether a PR already exists"
+)  # then we don't need to update the release notes
 args = parser.parse_args()
 
 text = []
 new_changelog_lines = []
 release_line = None
 
-text.append(f"Hello! This is an automated update of the following workflow: **{args.repo}**. I created this PR because I think one or more of the component tools are out of date, i.e. there is a newer version available on the ToolShed.\n")
-text.append("By comparing with the latest versions available on the ToolShed, it seems the following tools are outdated:")
+text.append(
+    f"Hello! This is an automated update of the following workflow: **{args.repo}**. I created this PR because I think one or more of the component tools are out of date, i.e. there is a newer version available on the ToolShed.\n"
+)
+text.append(
+    "By comparing with the latest versions available on the ToolShed, it seems the following tools are outdated:"
+)
 
 with open(args.log) as f:
     for n in f.readlines():
-        if ' -> ' in n:
+        if " -> " in n:
             text.append(f"* `{n.split()[1]}` should be updated to `{n.split()[3]}`")
-            new_changelog_lines.append(f"- `{n.split()[1]}` was updated to `{n.split()[3]}`")
-        if 'The workflow release number has been updated' in n:
+            new_changelog_lines.append(
+                f"- `{n.split()[1]}` was updated to `{n.split()[3]}`"
+            )
+        if "The workflow release number has been updated" in n:
             release_line = n
             text.append(f"\n{release_line}")
 
-with open(args.out, 'w') as f:
-    f.write('\n'.join(text))
+with open(args.out, "w") as f:
+    f.write("\n".join(text))
 
 if release_line:
     if not args.pr_exists:
-        with open(args.changelog, 'r+') as f:
+        with open(args.changelog, "r+") as f:
             lines = f.readlines()
-            new_change = [f"## [{release_line.split(' to ')[-1].strip().strip('.')}] " + str(date.today()), "", "### Automatic update"] + new_changelog_lines
-            new_lines = [lines[0]] + new_change + [''.join(lines[1:])]
+            new_change = [
+                f"## [{release_line.split(' to ')[-1].strip().strip('.')}] "
+                + str(date.today()),
+                "",
+                "### Automatic update",
+            ] + new_changelog_lines
+            new_lines = [lines[0]] + new_change + ["".join(lines[1:])]
             f.seek(0)
-            f.write('\n'.join(new_lines))
-    print(f"Updating {args.repo} {release_line.split('updated ')[-1].strip().strip('.')}")
+            f.write("\n".join(new_lines))
+    print(
+        f"Updating {args.repo} {release_line.split('updated ')[-1].strip().strip('.')}"
+    )
 else:
     print(f"Updating {args.repo}")
